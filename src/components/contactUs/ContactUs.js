215722e326058aa1ff8aefadcd1a8e6783e2acf5
Lentach <olek292@gmail.com>
2020-10-18 02:31:38 +0200

    form

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -3,7 +3,21 @@ import React from 'react';
 const ContactUs = () => {
     return (
         <div>
+            <form className='form'>
+                <h1>Contact from
+                    <i className="fab fa-phoenix-framework"/>
 
+                    <label>Name</label>
+                    <input placeholder='name'/>
+
+                    <label>Email</label>
+                    <input placeholder='Email'/>
+
+                    <label>Message</label>
+                    <textarea placeholder='message'/>
+                </h1>
+
+            </form>
         </div>
     );
 };
