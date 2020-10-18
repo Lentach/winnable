a64ee2b1856ef6d62490070ad0e73050e3e8b0da
Lentach <olek292@gmail.com>
2020-10-18 16:19:58 +0200

    react rorm

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ContactUs.css'
 const ContactUs = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e) => e.preventDefault()
+
     return (
         <div>
-            <form className='form'>
+            <form className='form' onSubmit={handleSubmit}>>
                 <h1>Contact Me</h1>
                 <i className="fab fa-phoenix-framework"/>
                 <br/>
                 <br/>
                     <label>Name</label>
-                    <input placeholder='Name'/>
+                    <input
+                        placeholder='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
 
                     <label>Email</label>
-                    <input placeholder='Email'/>
+                    <input
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
                     <label>Message</label>
-                    <textarea placeholder='message'/>
+                    <textarea
+                        placeholder='message'
+                        value={message}
+                        onChange={(e => setMessage(e.target.value))}
+                    />
 
                     <button type='submit'>Submit</button>
             </form>
